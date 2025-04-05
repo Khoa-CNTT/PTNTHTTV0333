068b935e3a835ff016c23cfd7809e236551fde4a
vohoang <vanhoang132003@gmail.com>
2025-04-05 13:47:05 +0700

    update

---
+++ Be/Meeting-BE/src/test/java/org/example/meetingbe/MeetingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.meetingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
