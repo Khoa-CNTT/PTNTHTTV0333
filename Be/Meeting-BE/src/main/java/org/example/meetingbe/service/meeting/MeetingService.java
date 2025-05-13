9de6a13473b404c7e50ca44a3b09f078c01ef4a0
DienTon <thatdien295@gmail.com>
2025-05-13 20:43:54 +0700

    up meeting history
    up meeting history

--- Be/Meeting-BE/src/main/java/org/example/meetingbe/service/meeting/MeetingService.java
+++ Be/Meeting-BE/src/main/java/org/example/meetingbe/service/meeting/MeetingService.java
@@ -2,18 +2,24 @@ package org.example.meetingbe.service.meeting;
 
 import org.example.meetingbe.dto.MeetingDto;
 import org.example.meetingbe.model.Meeting;
+import org.example.meetingbe.model.Participants;
 import org.example.meetingbe.repository.IMeetingRepo;
+import org.example.meetingbe.repository.IParticipantsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class MeetingService implements IMeetingService {
     @Autowired
     private IMeetingRepo meetingRepo;
+    @Autowired
+    private IParticipantsRepo participantsRepo;
 
     private final Map<String, MeetingDto> meetings = new HashMap<>();
     @Override
@@ -30,8 +36,10 @@ public class MeetingService implements IMeetingService {
     @Override
     public void addParticipant(String meetingId, Long userId) {
         MeetingDto meeting = meetings.get(meetingId);
+        Optional<Meeting> mt = meetingRepo.findById(meeting.getId());
         if (meeting != null) {
             meeting.getUser().add(userId);
+            Participants p = new Participants(LocalDateTime.now(),null,mt.get(),mt.get().getUser());
         }
     }
 }
--- Be/Meeting-BE/src/main/java/org/example/meetingbe/service/participants/ParticipantsService.java
+++ Be/Meeting-BE/src/main/java/org/example/meetingbe/service/participants/ParticipantsService.java
@@ -1,11 +1,31 @@
 package org.example.meetingbe.service.participants;
 
+import org.example.meetingbe.model.Contact;
+import org.example.meetingbe.model.Participants;
 import org.example.meetingbe.repository.IParticipantsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParticipantsService implements IParticipantsService {
     @Autowired
     private IParticipantsRepo participantsRepo;
+
+    @Override
+    public Participants findByUserIdAndMeetingId(Long userId, Long meetingId) {
+        return participantsRepo.findByUserIdAndMeetingId(userId,meetingId);
+    }
+
+    @Override
+    public void save(Participants participants) {
+        participantsRepo.save(participants);
+    }
+
+    @Override
+    public Page<Participants> getAll(Pageable pageable) {
+        Page<Participants> participants = participantsRepo.getAll(pageable);
+        return participants;
+    }
 }
